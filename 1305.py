2768afa73b84c0e1da6370f48e729af9520947c9
Igor03 <j.igor.ifce@gmail.com>
2020-02-16 19:20:38 -0300

    Adding problem 1305

---
+++ 1305.py
@@ -0,0 +1,27 @@
+# problem:
+# https://www.urionlinejudge.com.br/judge/en/problems/view/1305
+
+def get_int_frac_parts(string):
+    if '.' not in string:
+        return int(string), 0
+    else:
+        value = string.split('.')
+        if value[0] == '':
+            return 0, float('.'+value[1])
+        elif value[1] == '':
+            return int(value[0]), 0
+        else:
+            return int(value[0]), float('.'+value[1])
+
+
+while True:
+    try:
+        num = input()
+        cutoff = float(input())
+        values = get_int_frac_parts(num)
+        if values[1] >= cutoff:                        
+            print(values[0]+1)            
+        else:
+            print(values[0])
+    except EOFError:
+        break
